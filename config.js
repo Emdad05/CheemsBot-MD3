a091ac0f0a7c869016d3c80295f08fcf2f2aa93c
Emdad05 <99329205+Emdad05@users.noreply.github.com>
2022-07-06 08:58:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -41,26 +41,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['ur number'] //ur owner number
-global.ownername = "ur name"
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['Emd🤖'] //ur owner number
+global.ownername = "Emdad Hussain"
+global.ytname = "YT: Nothing" //ur yt chanel name
+global.socialm = "Insta: emdad_98760" //ur github or insta name
+global.location = "India, Assam" //ur location
 
 //bot bombdy
-global.botname = "ur bot name" //ur bot name
-global.websitex = "https://youtu.be/xn9RatOrbuI" //ur website
+global.botname = "Emd🤖" //ur bot name
+global.websitex = "https://chat.whatsapp.com/DlIvZ6YMSVQ5hHxt2NuzDA" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/zlzQZvXH.mp4' } //gif and video menu
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
-global.author = "🦄Dream Guy Xeon"
+global.packname = "Emd🤖 Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
+global.author = "Emdad Hussain"
 global.themeemoji = "🐶"
 global.reactmoji = "😘"
-global.ownertag = ['ur number'] //ur owner tag
-global.ownernummenu = ['ur number'] //ur owner number in menu and all
-global.watermark = "Xeon Bot Inc." //ur watermark
-global.botscript = "https://github.com/DGXeon/CheemsBot-MD3"
-global.linkz1 = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS"
-global.linkz2 = "https://chat.whatsapp.com/EcycNbJFCVT5ZsG9xIGkqd"
+global.ownertag = ['917099915088'] //ur owner tag
+global.ownernummenu = ['917099915088'] //ur owner number in menu and all
+global.watermark = "Emd Bot Inc." //ur watermark
+global.botscript = "https://github.com/Emdad05/CheemsBot-MD3"
+global.linkz1 = "https://chat.whatsapp.com/DlIvZ6YMSVQ5hHxt2NuzDA"
+global.linkz2 = "https://chat.whatsapp.com/DlIvZ6YMSVQ5hHxt2NuzDA"
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
